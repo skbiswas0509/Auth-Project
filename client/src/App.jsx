c3b84c23f070ed07a47f99bc8de941123ab648cc
Sukumar Biswas <sukumarbsws98@gmail.com>
2025-04-19 10:14:11 +0600

    starting of login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import ResetPassword from './pages/ResetPassword'
+import EmailVerify from './pages/EmailVerify'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/reset-password' element={<ResetPassword/>}/>
+        <Route path='/email-verify' element={<EmailVerify/>}/>
+      </Routes>
+    </div>
   )
 }
 
